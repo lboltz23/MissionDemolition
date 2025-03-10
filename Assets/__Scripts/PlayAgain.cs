ce0fbdfacc2521265363c725de1ff9108541ead3
lboltz23 <lacey.boltz@gmail.com>
2025-03-09 20:52:26 -0500

    pushed all files

---
+++ Assets/__Scripts/PlayAgain.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayAgain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnClick(){
+        SceneManager.LoadScene("_Scene_0");
+    }
+}
---
+++ Assets/__Scripts/Slingshot.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    [SerializeField] private LineRenderer rubberBand;
+    [SerializeField] private Transform firstpoint;
+    [SerializeField] private Transform secondpoint;
+
+    [Header("Inscribed")]
+    public GameObject projectilePrefab;
+    public float velocityMult = 10f;
+    public GameObject projLinePrefab;
+
+    [Header("Dynamic")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    public AudioClip launch;
+    
+    
+
+    Vector3 GetMousePositionInWorld(){
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mouseinworld = Camera.main.ScreenToWorldPoint(mousePos2D);
+        return mouseinworld;
+    }
+
+    void Start()
+    {
+       rubberBand.SetPosition(0, firstpoint.position);
+       rubberBand.SetPosition(2, secondpoint.position);
+
+    }
+    void Awake(){
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive( false );
+        launchPos = launchPointTrans.position;
+
+        
+    }
+    // Start is called before the first frame update
+    void OnMouseEnter(){
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive( true );
+    }
+
+    void OnMouseExit(){
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive( false );
+    }
+
+    void OnMouseDown(){
+        aimingMode = true;
+        projectile = Instantiate( projectilePrefab ) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    void Update(){
+        if(!aimingMode) return;
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 mouseDelta = mousePos3D -launchPos;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if(mouseDelta.magnitude > maxMagnitude){
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if(Input.GetMouseButtonUp(0)){
+            aimingMode = false;
+            Rigidbody projRB = projectile.GetComponent<Rigidbody>();
+            projRB.isKinematic = false;
+            projRB.collisionDetectionMode = CollisionDetectionMode.Continuous;
+            projRB.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
+            Instantiate<GameObject>(projLinePrefab, projectile.transform);
+            projectile = null;
+            MissionDemolition.SHOT_FIRED();
+
+            AudioSource.PlayClipAtPoint(launch, transform.position);
+        }
+
+        if(Input.GetMouseButton(0)){
+            rubberBand.SetPosition(1, GetMousePositionInWorld());
+
+        } 
+        
+    }
+}
